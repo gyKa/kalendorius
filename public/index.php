94e11b4f29e766587b6f639a7e4c5ec4daf1c141
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-06-03 11:42:22 +0300

    Add debugging mode based on ENV file.

--- public/index.php
+++ public/index.php
@@ -2,7 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
+$dotenv->required('DEBUG')->notEmpty();
+
 $app = new Silex\Application();
+$app['debug'] = getenv('DEBUG') === 'true'? true: false;
 
 $app->get('/', function () {
     return 'Hello World!';
